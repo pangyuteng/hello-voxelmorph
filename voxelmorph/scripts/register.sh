b187888ebf6e930e3c6d43d1e22d1c8d069f82ab
pangyuteng <pangyuteng@gmail.com>
2024-02-24 09:27:04 -0800

    increased memory

--- voxelmorph/scripts/register.sh
+++ voxelmorph/scripts/register.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "$@"
+fixed_file=$1
+moving_file=$2
+moved_file=$3
 
 cd /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts
-$@
+python register.py --size "(128,128,128)" --gpu 0 --model shapes-dice-vel-3-res-8-16-32-256f.h5 \
+    --fixed ${fixed_file} --moving ${moving_file} --moved ${moved_file}
