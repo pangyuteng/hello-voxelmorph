c7ec330e8ec2ba85f6e43cf171a8b244ab2fa060
pangyuteng <pangyuteng@gmail.com>
2024-08-24 09:28:57 -0700

    wip debug

--- chest-ct-tlc-rc/hola.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -it -u $(id -u):$(id -g) -w $PWD -v /cvibraid:/cvibraid pangyuteng/voxelmorph:latest \
-bash -c "bash test/test_register_transform.sh"
---
+++ chest-ct-tlc-rc/test/test.sh
@@ -0,0 +1,7 @@
+
+
+python ../register_transform.py input.json
+
+
+#docker run -it -u $(id -u):$(id -g) -w $PWD -v /cvibraid:/cvibraid pangyuteng/voxelmorph:latest 
+#bash test/test.sh
