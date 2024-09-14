5d6ddbe7f96ec518206239fe3ff53e091047f5be
pangyuteng <pangyuteng@gmail.com>
2024-09-14 07:57:43 -0700

    wip

--- synthmorph_wrapper/setup.py
+++ synthmorph_wrapper/setup.py
@@ -6,15 +6,12 @@ setuptools.setup(name='synthmorph_wrapper',
     url='https://github.com/pangyuteng/hello-voxelmorph/tree/main/wrapper',
     author='pangyuteng',
     license='MIT',
-    packages=setuptools.find_packages(),
+    py_modules=["synthmorph_wrapper"],
     install_requires=[
         'voxelmorph',
         'SimpleITK-SimpleElastix',
     ],
-    py_modules=["synthmorph_wrapper"],
-    package_dir={'':'synthmorph_wrapper'}, 
-    include_package_data=True,
-    zip_safe=False)
+)
 
 """
 
