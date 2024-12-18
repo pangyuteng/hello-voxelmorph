634fa6e9c8f34a5cf22d683abdf386b8471a5313
pangyuteng <pangyuteng@gmail.com>
2024-12-18 18:07:39 +0000

    updated test script to store image as nifti first

--- synthmorph-wrapper/test/test.py
+++ synthmorph-wrapper/test/test.py
@@ -1,10 +1,24 @@
-
+import os
 import sys
 import json
+import SimpleITK as sitk
 from synthmorph_wrapper import register_transform as rt
 
+# json_file = sys.argv[1]
+def dcm2nifti(folder_path):
+    nifti_file = os.path.join(folder_path,"image.nii.gz")
+    if not os.path.exists(nifti_file):
+        dcm_list = [(int(x.split("-")[0]),x) for x in os.listdir(folder_path) if x.endswith(".dcm")]
+        dcm_list = [os.path.join(folder_path,x[1]) for x in sorted(dcm_list,key=lambda x:x[0])]
+        reader = sitk.ImageSeriesReader()
+        reader.SetFileNames(dcm_list)
+        img_obj = reader.Execute()
+        sitk.WriteImage(img_obj,nifti_file)
 
-json_file = sys.argv[1]
+dcm2nifti("image-1")
+dcm2nifti("image-2")
+
+json_file = "input.json"
 
 with open(json_file,'r') as f:
     content_dict = json.loads(f.read())
@@ -17,3 +31,4 @@ docker run -it -u $(id -u):$(id -g) -v $PWD:/test -w /test pangyuteng/synthmorph
 python test.py input.json
 
 """
+
