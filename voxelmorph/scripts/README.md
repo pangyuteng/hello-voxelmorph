be05789c6f1ccbfbb6dfe2a20bf4bd285240fdeb
pangyuteng <pteng@mednet.ucla.edu>
2025-02-24 15:26:11 -0800

    wip

--- voxelmorph/scripts/README.md
+++ voxelmorph/scripts/README.md
@@ -33,3 +33,15 @@ tl_masked.csv
 
 
 python convert_tl_to_imagelist.py
+
+
+
+```
+https://github.com/voxelmorph/voxelmorph/issues/271
+http://tutorial.voxelmorph.net
+
+docker run -it -u $(id -u):$(id -g) \
+    -w $PWD -v /cvibraid:/cvibraid -v /radraid:/radraid \
+    pangyuteng/voxelmorph:latest bash
+
+```
--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0 python /opt/voxelmorph/scripts/tf/train.py \
-    --img-list tl.csv --gpu 0  --batch-size 2
+    --img-list tl.csv \
+    --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
+    --gpu 0 --batch-size 2 \
+    --int-downsize 0
+
+#
+# --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5 \
