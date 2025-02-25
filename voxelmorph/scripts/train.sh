5428f1d7579e5c2b0bbad8dc9eba3b5e722bb122
pangyuteng <pteng@mednet.ucla.edu>
2025-02-25 09:07:15 -0800

    added --load-weights

--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -5,7 +5,5 @@ CUDA_VISIBLE_DEVICES=0 python \
     --img-list tl.csv \
     --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
     --gpu 0 --batch-size 6 \
-    --int-downsize 0
-
-#
-# --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5 \
+    --int-downsize 0 \
+    --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir/0000.h5
