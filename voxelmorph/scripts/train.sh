ac1473fead2ef611fa9a76c20b3ba98a7aecd188
pangyuteng <pteng@mednet.ucla.edu>
2025-02-24 20:00:42 -0800

    reduced batchsize

--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -4,8 +4,9 @@ CUDA_VISIBLE_DEVICES=0 python \
     /opt/train.py \
     --img-list tl.csv \
     --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
-    --gpu 0 --batch-size 8 \
+    --gpu 0 --batch-size 2 \
     --int-downsize 0
 
+#--gpu 0 --batch-size 8 \
 #
 # --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5 \
