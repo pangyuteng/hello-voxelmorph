0cbbae5143dfddfd9cbe41651f8d06e883f31e04
pangyuteng <pteng@mednet.ucla.edu>
2025-02-24 16:20:05 -0800

    wip

--- voxelmorph/scripts/convert_tl_to_imagelist.py
+++ voxelmorph/scripts/convert_tl_to_imagelist.py
@@ -1,9 +1,11 @@
-
+import os
 import pandas as pd
 df = pd.read_csv("tl_masked.csv")
 
 with open("tl.csv","w") as f:
     for x in df['nifti_file']:
+        if os.path.exists(x) is False:
+            continue
         f.write(x+"\n")
 
 """
