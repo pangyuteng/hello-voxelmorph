bbaef9ba31aae2682adcc1dc14da9efece9379a9
pangyuteng <pteng@mednet.ucla.edu>
2025-02-26 14:43:45 -0800

    set gpu=1 for l40

--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python \
+CUDA_VISIBLE_DEVICES=1 python \
     /opt/train.py \
     --img-list tl.csv \
     --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
-    --gpu 0 --batch-size 2 \
+    --gpu 1 --batch-size 2 \
     --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5
 
 # --enc 256 256 256 256 \
