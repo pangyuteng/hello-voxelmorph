66a046740b1e90c9a809f778764895cd584d559d
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-10-23 11:05:15 -0700

    wip

--- voxelmorph/scripts/register.sh
+++ voxelmorph/scripts/register.sh
@@ -4,9 +4,10 @@ echo "$@"
 fixed_file=$1
 moving_file=$2
 moved_file=$3
+weight_file=$4
 
 CUDA_VISIBLE_DEVICES=0 python \
     /opt/register.py \
     --gpu 0 \
-    --model  /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/1220.h5 \
+    --model ${weight_file} \
     --fixed ${fixed_file} --moving ${moving_file} --moved ${moved_file}
