44805a72b52021a957d9f122f74889434f6fef8a
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-10-23 16:38:27 -0700

    wip

--- voxelmorph/scripts/register.sh
+++ voxelmorph/scripts/register.sh
@@ -9,5 +9,5 @@ weight_file=$4
 CUDA_VISIBLE_DEVICES=0 python \
     /opt/register.py \
     --gpu 0 \
-    --model ${weight_file} \
-    --fixed ${fixed_file} --moving ${moving_file} --moved ${moved_file}
+    --fixed ${fixed_file} --moving ${moving_file} \
+    --moved ${moved_file} --model ${weight_file}
--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -5,8 +5,10 @@ CUDA_VISIBLE_DEVICES=0 python \
     --img-list tl.csv \
     --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
     --gpu 1 --batch-size 2 \
-    --initial-epoch 1140 \
-    --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir/1140.h5
+    --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5
+
+#    --initial-epoch 1140 \
+#    --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir/1140.h5
 
 #    --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5
 
