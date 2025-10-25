37b9ebbd882e3ae2562a2b94489009c89af9d13a
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-10-25 15:59:32 -0700

    wip

--- voxelmorph/scripts/train.sh
+++ voxelmorph/scripts/train.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python \
     /opt/train.py \
     --img-list tl.csv \
     --model-dir /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/workdir \
-    --gpu 1 --batch-size 2 \
+    --gpu 0 --batch-size 2 \
     --load-weights /cvibraid/cvib2/apps/personal/pteng/github/hello-voxelmorph/voxelmorph/scripts/shapes-dice-vel-3-res-8-16-32-256f.h5
 
 #    --initial-epoch 1140 \
