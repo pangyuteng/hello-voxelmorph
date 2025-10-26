cd5097fe66c3132ac5c5e3198793a22debed8867
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-10-26 12:49:54 -0700

    wip register.py

--- voxelmorph/register.py
+++ voxelmorph/register.py
@@ -61,24 +61,20 @@ fixed, fixed_affine = vxm.py.utils.load_volfile(
     args.fixed, add_batch_axis=True, add_feat_axis=add_feat_axis, ret_affine=True)
 """
 
-def myload(nifti_file):
-    # super custom rescale and resize
-    minval,maxval = -1000,1000
-    TARGET_SZ = 128
-    TARGET_SHAPE = [TARGET_SZ,TARGET_SZ,TARGET_SZ]
-
+def myload(nifti_file,minval=-1000,maxval=1000,out_minval=0,out_maxval=1,target_sz=128):
+    target_shape = [target_sz,target_sz,target_sz]
     img_obj = nib.load(nifti_file)
     moving_shape_np = np.array(img_obj.shape).astype(np.float32)
     moving_spacing_np = np.array(img_obj.header.get_zooms()).astype(np.float32)
-    target_shape_np = np.array(TARGET_SHAPE).astype(np.float32)
+    target_shape_np = np.array(target_shape).astype(np.float32)
     target_spacing_np = moving_shape_np*moving_spacing_np/target_shape_np
     moving_resize_factor = moving_spacing_np/target_spacing_np
     # interesting `+1` in vox2out_vox https://github.com/nipy/nibabel/issues/1366
-    sm_img_obj = resample_to_output(img_obj,voxel_sizes=target_spacing_np)
-    sm_img = sm_img_obj.get_fdata()[:TARGET_SZ,:TARGET_SZ,:TARGET_SZ].astype(np.float32)
-    sm_img = ( (sm_img-minval)/(maxval-minval) ).clip(0,1)
-    sm_img = sm_img[np.newaxis, ... , np.newaxis]
-    return sm_img, sm_img_obj.affine
+    out_img_obj = resample_to_output(img_obj,voxel_sizes=target_spacing_np)
+    out_img = out_img_obj.get_fdata()[:target_sz,:target_sz,:target_sz].astype(np.float32)
+    out_img = ( (out_img-minval)/(maxval-minval) ).clip(out_minval,out_maxval)
+    out_img = out_img[np.newaxis, ... , np.newaxis]
+    return out_img, out_img_obj.affine
 
 moving, _ = myload(args.moving)
 fixed, fixed_affine = myload(args.fixed)
@@ -92,31 +88,32 @@ with tf.device(device):
     warp = vxm.networks.VxmDense.load(args.model, **config).register(moving, fixed)
     moved = vxm.networks.Transform(inshape, nb_feats=nb_feats).predict([moving, warp])
 
-"""
-    is_mask = False
-    if is_mask:
-        interp_method = 'nearest'
-    else:
-        interp_method = 'linear'
-
-    lg_moved = vxm.networks.Transform(lg_inshape,
-        rescale=RESCALE_FACTOR,
-        nb_feats=nb_feats,
-        interp_method=interp_method).predict([lg_moving, warp])
-
-    vxm.py.utils.save_volfile(lg_moved.squeeze(), lg_moved_file, lg_fixed_affine)
-"""
-
 # save warp
 if args.warp:
     vxm.py.utils.save_volfile(warp.squeeze(), args.warp, fixed_affine)
 
 minval,maxval = -1000,1000
-print(np.min(moved),np.max(moved),'!!!')
 moved = (moved.clip(0,1)*(maxval-minval))+minval
 moved = moved.astype(np.int32)
-# save moved image
 vxm.py.utils.save_volfile(moved.squeeze(), args.moved, fixed_affine)
+
+if False:
+    rescale_factor = 4
+    interp_method = 'linear'
+
+    lg_moving, _ = myload(args.moving,target_sz=512)
+    lg_fixed, lg_fixed_affine = myload(args.fixed,target_sz=512)
+    lg_inshape = lg_fixed.shape[1:-1]
+
+    lg_moved = vxm.networks.Transform(lg_inshape,
+        rescale=rescale_factor,
+        nb_feats=nb_feats,
+        interp_method=interp_method).predict([lg_moving, warp])
+
+    lg_moved = (lg_moved.clip(0,1)*(maxval-minval))+minval
+    lg_moved = lg_moved.astype(np.int32)
+    vxm.py.utils.save_volfile(lg_moved.squeeze(), args.moved, lg_fixed_affine)
+
 
 """
 
