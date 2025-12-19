0a834cc72b0946729ff89d53f73f9a8b4b8d8e49
pangyuteng <pangyuteng@gmail.com>
2025-12-19 22:14:25 +0000

    wip

--- voxelmorph/mylosses.py
+++ voxelmorph/mylosses.py
@@ -1,5 +1,13 @@
 
 raise NotImplementedError()
+#
+# D Wang, PLOSL: Population Learning Followed by One Shot Learning Pulmonary Image Registration 
+# Using Tissue Volume Preserving and Vesselness Constraints
+# https://pmc.ncbi.nlm.nih.gov/articles/PMC11225793
+#
+# Y Yin, Mass preserving nonrigid registration of CT lung images using cubic B-spline
+# https://pmc.ncbi.nlm.nih.gov/articles/PMC2749644
+#
 
 # max hu 1000, min hu = -1000
 # input max is 1, min is 0
