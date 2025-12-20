dea5b4cf8190e02e34d5016d6c7b38120035c307
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-12-20 15:38:59 -0800

    voxelmorph 3.0

--- voxelmorph/build_and_push.sh
+++ voxelmorph/build_and_push.sh
@@ -1,5 +1,5 @@
 docker build -t pangyuteng/voxelmorph:0.1.2-torch -f Dockerfile.torch .
-docker push pangyuteng/voxelmorph:0.1.2-torch
+#docker push pangyuteng/voxelmorph:0.1.2-torch
 
-docker build -t pangyuteng/voxelmorph:0.1.2-tf -f Dockerfile.tf .
-docker push pangyuteng/voxelmorph:0.1.2-tf
+#docker build -t pangyuteng/voxelmorph:0.1.2-tf -f Dockerfile.tf .
+#docker push pangyuteng/voxelmorph:0.1.2-tf
