866f624e9cdbd882f5aa346701e6999457f1fa4b
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-12-20 19:32:03 -0800

    wip

--- voxelmorph/tf/build_and_push.sh
+++ voxelmorph/tf/build_and_push.sh
@@ -1,2 +1,5 @@
-docker build -t pangyuteng/voxelmorph:0.1.2-tf -f Dockerfile .
+#docker build -t pangyuteng/voxelmorph:0.1.2-tf -f Dockerfile .
+#docker push pangyuteng/voxelmorph:0.1.2-tf
+
+docker build -t pangyuteng/voxelmorph:0.1.2-tf -f Dockerfile.keras224 .
 docker push pangyuteng/voxelmorph:0.1.2-tf
