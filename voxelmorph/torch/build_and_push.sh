51489750cab61e0e1c5cea363e1ba679d8c6e7aa
pangyuteng <pangyuteng@gmail.com>
2025-12-23 16:22:11 +0000

    wip

--- voxelmorph/torch/build_and_push.sh
+++ voxelmorph/torch/build_and_push.sh
@@ -1,2 +1,2 @@
-docker build -t pangyuteng/voxelmorph:0.1.2-torch -f Dockerfile.torch .
+docker build -t pangyuteng/voxelmorph:0.1.2-torch -f Dockerfile .
 docker push pangyuteng/voxelmorph:0.1.2-torch
