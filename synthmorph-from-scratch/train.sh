c8a6c549a566c14153444ab5aa53bcb3e00132ac
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2026-01-16 17:35:42 -0800

    wip log folder

--- synthmorph-from-scratch/train.sh
+++ synthmorph-from-scratch/train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 /opt/multimodal-registration/train_synthmorph.py --config-path config.json
+python3 /opt/multimodal-registration/train_synthmorph.py --config-path config-single-gpu.json
+#python3 /opt/multimodal-registration/train_synthmorph.py --config-path config.json
