891ce322b40089d8ea1000f35c3adbe1fedc04c5
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2026-01-19 09:24:55 -0800

    wip

--- voxelmorph/tf/build_and_push.sh
+++ voxelmorph/tf/build_and_push.sh
@@ -1,8 +1,11 @@
-docker build -t pangyuteng/voxelmorph:0.1.2-tf-1.19 -f Dockerfile.tf.1.19 .
+#docker build -t pangyuteng/voxelmorph:0.1.2-tf-1.19 -f Dockerfile.tf.1.19 .
 #docker push pangyuteng/voxelmorph:0.1.2-tf-1.19
 
-docker build -t pangyuteng/voxelmorph:0.1.2-tf-2.4 -f Dockerfile.tf.2.4 .
+#docker build -t pangyuteng/voxelmorph:0.1.2-tf-2.4 -f Dockerfile.tf.2.4 .
 #docker push pangyuteng/voxelmorph:0.1.2-tf-2.4
 
-docker build -t pangyuteng/voxelmorph:0.1.2-tf-1.11 -f Dockerfile.tf.1.11 .
+#docker build -t pangyuteng/voxelmorph:0.1.2-tf-1.11 -f Dockerfile.tf.1.11 .
 #docker push pangyuteng/voxelmorph:0.1.2-tf-1.11
+
+docker build -t pangyuteng/voxelmorph:0.1.2-tf-2.19 -f Dockerfile.tf.2.19 .
+docker push pangyuteng/voxelmorph:0.1.2-tf-2.19
