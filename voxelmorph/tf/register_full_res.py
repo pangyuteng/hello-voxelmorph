067ecbf118c7ee6a7eb3697e561ad429c03de7cb
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2026-01-19 14:58:26 -0800

    refactor

--- voxelmorph/tf/register_full_res.py
+++ voxelmorph/tf/register_full_res.py
@@ -11,6 +11,47 @@ from skimage.measure import label, regionprops
 from nibabel.processing import resample_to_output, resample_from_to
 from voxelmorph.py.utils import jacobian_determinant
 
+def cleanup_mask(org_mask_obj):
+    org_mask = org_mask_obj.get_fdata()
+    new_mask = np.zeros_like(org_mask)
+    for idx in np.unique(org_mask):
+        if idx <= 0:
+            continue
+        label_image = label(org_mask==idx)
+        region_list = regionprops(label_image)
+        if len(region_list) > 1: # get largest
+            print(f"cleaning mask {idx}")
+            region_list = sorted(region_list,key=lambda x: x.area,reverse=True)
+            largest_blob = (label_image == region_list[0].label).astype(np.uint8)
+            new_mask[largest_blob==1]=idx
+        else:
+            print(f"mask clean not needed {idx}")
+            new_mask[org_mask==idx]=idx
+    new_mask_obj = nib.Nifti1Image(new_mask, org_mask_obj.affine)
+    return new_mask_obj
+
+def myload(nifti_file,minval=-1000,maxval=1000,out_minval=0,out_maxval=1,target_sz=128,scale_intensity=True):
+    order = 3 if scale_intensity else 0
+    target_shape = [target_sz,target_sz,target_sz]
+    img_obj = nib.load(nifti_file)
+    moving_shape_np = np.array(img_obj.shape).astype(np.float32)
+    moving_spacing_np = np.array(img_obj.header.get_zooms()).astype(np.float32)
+    target_shape_np = np.array(target_shape).astype(np.float32)
+    target_spacing_np = moving_shape_np*moving_spacing_np/target_shape_np
+    moving_resize_factor = moving_spacing_np/target_spacing_np
+    # interesting `+1` in vox2out_vox https://github.com/nipy/nibabel/issues/1366
+    out_img_obj = resample_to_output(img_obj,voxel_sizes=target_spacing_np,cval=minval,order=order)
+    # hack to get desired shape
+    new_img = nib.Nifti1Image(np.zeros(target_shape), out_img_obj.affine, out_img_obj.header)
+    out_img_obj = resample_from_to(img_obj,new_img,cval=minval,order=order)
+    out_img = out_img_obj.get_fdata()
+    #out_img = out_img_obj.get_fdata()[:target_sz,:target_sz,:target_sz].astype(np.float32)
+    if scale_intensity:
+        out_img = ( (out_img-minval)/(maxval-minval) ).clip(out_minval,out_maxval)
+    out_img = out_img[np.newaxis, ... , np.newaxis]
+    return out_img_obj, out_img, out_img_obj.affine
+
+
 # parse commandline args
 parser = argparse.ArgumentParser()
 parser.add_argument('--moving', required=True, help='moving image (source) filename')
@@ -33,27 +74,6 @@ args = parser.parse_args()
 #assert(args.gpu is None) # ensure using CPU
 # tensorflow device handling
 device, nb_devices = vxm.utils.setup_device(args.gpu)
-
-def myload(nifti_file,minval=-1000,maxval=1000,out_minval=0,out_maxval=1,target_sz=128,scale_intensity=True):
-    order = 3 if scale_intensity else 0
-    target_shape = [target_sz,target_sz,target_sz]
-    img_obj = nib.load(nifti_file)
-    moving_shape_np = np.array(img_obj.shape).astype(np.float32)
-    moving_spacing_np = np.array(img_obj.header.get_zooms()).astype(np.float32)
-    target_shape_np = np.array(target_shape).astype(np.float32)
-    target_spacing_np = moving_shape_np*moving_spacing_np/target_shape_np
-    moving_resize_factor = moving_spacing_np/target_spacing_np
-    # interesting `+1` in vox2out_vox https://github.com/nipy/nibabel/issues/1366
-    out_img_obj = resample_to_output(img_obj,voxel_sizes=target_spacing_np,cval=minval,order=order)
-    # hack to get desired shape
-    new_img = nib.Nifti1Image(np.zeros(target_shape), out_img_obj.affine, out_img_obj.header)
-    out_img_obj = resample_from_to(img_obj,new_img,cval=minval,order=order)
-    out_img = out_img_obj.get_fdata()
-    #out_img = out_img_obj.get_fdata()[:target_sz,:target_sz,:target_sz].astype(np.float32)
-    if scale_intensity:
-        out_img = ( (out_img-minval)/(maxval-minval) ).clip(out_minval,out_maxval)
-    out_img = out_img[np.newaxis, ... , np.newaxis]
-    return out_img_obj, out_img, out_img_obj.affine
 
 sm_moving_obj, moving, _ = myload(args.moving)
 sm_fixed_obj, fixed, fixed_affine = myload(args.fixed)
@@ -116,25 +136,6 @@ if args.moved:
     #reshape this back
     lg_moved_obj = resample_from_to(lg_moved_obj,fixed_obj)
     nib.save(lg_moved_obj, args.moved)
-
-def cleanup_mask(org_mask_obj):
-    org_mask = org_mask_obj.get_fdata()
-    new_mask = np.zeros_like(org_mask)
-    for idx in np.unique(org_mask):
-        if idx <= 0:
-            continue
-        label_image = label(org_mask==idx)
-        region_list = regionprops(label_image)
-        if len(region_list) > 1: # get largest
-            print(f"cleaning mask {idx}")
-            region_list = sorted(region_list,key=lambda x: x.area,reverse=True)
-            largest_blob = (label_image == region_list[0].label).astype(np.uint8)
-            new_mask[largest_blob==1]=idx
-        else:
-            print(f"mask clean not needed {idx}")
-            new_mask[org_mask==idx]=idx
-    new_mask_obj = nib.Nifti1Image(new_mask, org_mask_obj.affine)
-    return new_mask_obj
 
 if args.moving_mask:
     myfolder = os.path.dirname(args.moving_mask)
